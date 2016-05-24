744a450b3618d30c7807e30f7475b4f5036927a7
Dmytro <dbogatov@wpi.edu>
2016-05-24 16:19:30 +0300

    Start cart.

---
+++ src/EShop/Controllers/API/CartController.cs
@@ -0,0 +1,14 @@
+using EShop.Models.Enitites;
+using EShop.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers.API
+{
+
+	[Produces("application/json")]
+	[Route("api/Cart")]
+	public class CartController : Controller
+	{
+
+	}
+}
