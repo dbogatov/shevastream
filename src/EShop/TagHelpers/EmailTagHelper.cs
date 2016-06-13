37d8b0034d05234c2f59a612d2942e47305db13a
Dmytro <dbogatov@wpi.edu>
2016-06-13 15:14:04 +0300

    Add email tag helper.

---
+++ src/EShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace EShop.TagHelpers
+{
+    [HtmlTargetElement("email", Attributes = "to")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string To { get; set; }
+        public string Subject { get; set; } = "";
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.TagMode = TagMode.StartTagAndEndTag;
+
+			output.Attributes.Clear();
+			output.Attributes.Add("href", $"mailto:{To}?subject={Subject.Replace(" ", "%20")}");
+			output.Attributes.Add("target", "_top");
+
+			output.Content.SetContent(To);
+		}
+    }
+}
