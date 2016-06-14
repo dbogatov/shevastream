9723d38c73fd6fbf9f4248daf932ab4e643a5745
Dmytro <dbogatov@wpi.edu>
2016-06-14 15:32:15 +0300

    Minify js and css.

--- src/EShop/wwwroot/js/script.js
+++ src/EShop/wwwroot/js/script.js
@@ -1,25 +1,5 @@
 $(document).ready(function() {
 
-	// Contact Maps
-	$("#maps").gmap3({
-		map: {
-			options: {
-				center: [50.441892, 30.511544],
-				zoom: 15,
-				scrollwheel: false
-			}
-		},
-		marker: {
-			latLng: [50.441892, 30.511544],
-			options: {
-				icon: new google.maps.MarkerImage(
-					"/images/location.png",
-					new google.maps.Size(48, 48, "px", "px")
-				)
-			}
-		}
-	});
-
 	//Slider
 	$("#slider").carousel({
 		interval: 5000
